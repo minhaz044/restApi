49e7ae6528c4d7550bfe8d686a8ea3dfd5ac65b4
Minhaz Uddin <30874666+minhaz044@users.noreply.github.com>
2023-03-23 15:19:13 +0600

    Update RestApiApplication.java

--- src/main/java/com/minhaz/restApi/RestApiApplication.java
+++ src/main/java/com/minhaz/restApi/RestApiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RestApiApplication {
 
 	public static void main(String[] args) {
-		// Hello this is a simple commit
+		// Hello this is a simple commit...
 		SpringApplication.run(RestApiApplication.class, args);
 	}
 
